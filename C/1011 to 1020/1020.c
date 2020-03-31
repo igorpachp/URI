8029c26c95dcec1ecf5f6b61dc10fc7e59eca596
Igor <igorpacheco9@gmail.com>
2020-03-31 20:06:25 -0300

    file format fixes

--- C/1011 to 1020/1020.c
+++ C/1011 to 1020/1020.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int IdadeD, IdadeA, IdadeM;
-    scanf("%d", &IdadeD);
-    IdadeA = IdadeD/365;
-    IdadeM = (IdadeD%365)/30;
-    IdadeD = (IdadeD%365)%30;
-    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", IdadeA, IdadeM, IdadeD);
+	int IdadeD, IdadeA, IdadeM;
 
-    return 0;
+	scanf("%d", &IdadeD);
+	IdadeA = IdadeD/365;
+	IdadeM = (IdadeD%365)/30;
+	IdadeD = (IdadeD%365)%30;
+	printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", IdadeA, IdadeM, IdadeD);
+
+	return 0;
 }
